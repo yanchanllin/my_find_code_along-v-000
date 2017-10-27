50918d8049cf6afb5992aa99492c539c85c1ad5a
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 16:59:34 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,8 @@ def my_find(collection)
   i = 0
     while i < collection.length
       if yield(collection[i])
-        binding.pry
         return collection[i]
-         
+
       end
       i = i + 1
     end
